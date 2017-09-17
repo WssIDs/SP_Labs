710bae8fb33294d26b5f22724865cce9060b8042
WssIDs <wssids49@gmail.com>
2017-09-17 10:34:23 +0300

    Лабораторная работа №1 Готовность

--- Lab_01/Source.cpp
+++ Lab_01/Source.cpp
@@ -6,11 +6,6 @@ LRESULT CALLBACK SimWndProc(HWND, UINT, WPARAM, LPARAM);
 
 
 //-- Global Variables ------------ 
-int g_rectRight = 0;
-int g_rectBottom = 0;
-bool g_lmousedown = false;
-int g_rectLeft = 0;
-int g_rectTop = 0;
 
 
 //  Стартовая функция 
@@ -19,7 +14,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 {
 	WNDCLASSEX wc;
 	MSG msg;
-	HWND hWnd,hWnd1;
+	HWND hWnd;
 
 	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 	wc.cbSize = sizeof(WNDCLASSEX);
@@ -67,32 +62,9 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		return FALSE;
 	}
 
-	hWnd1 = CreateWindowEx(NULL, wc.lpszClassName,
-		TEXT("Володько Виталий Иванович rfds"),
-		Stl,
-		200,
-		200,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
-
-	if (!hWnd1)
-	{
-		MessageBox(NULL, TEXT("Окно не создано!"),
-			TEXT("Ошибка"), MB_OK | MB_ICONERROR);
-		return FALSE;
-	}
-
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
-
-	ShowWindow(hWnd1, nCmdShow);
-	UpdateWindow(hWnd1);
 
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
@@ -105,91 +77,15 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 LRESULT CALLBACK SimWndProc(HWND hWnd, UINT msg,
 	WPARAM wParam, LPARAM lParam)
 {
-
 	HDC hDC;
-	RECT Rect;//до этого писалось в никуда, теперь будет писаться в эту структуру.
-	RECT tRect;
-
 
 	switch (msg)
 	{
-	case WM_LBUTTONDOWN:
-	{
-		//GetClientRect(hWnd, &Rect);//передаем указатель на нее
-
-		HDC hDc;
-		hDc = GetDC(hWnd);
-		DWORD x, y;
-		x = LOWORD(lParam);
-		y = HIWORD(lParam);
-
-		g_rectLeft = x;
-		g_rectTop = y;
-
-		InvalidateRect(hWnd, &Rect, TRUE);//и тут
-		//Rectangle(hDc, 50, 50, a, b);
-
-		g_lmousedown = true;
-
-	}
-		return 0;
-	
-	case WM_LBUTTONUP:
-	{
-		g_lmousedown = false;
-	}
-	return 0;
-
-
-	case WM_MOUSEMOVE:
-	{		
-		if (g_lmousedown == true)
-		{
-			GetClientRect(hWnd, &Rect);//передаем указатель на нее
-
-			HDC hDc;
-			hDc = GetDC(hWnd);
-			DWORD x, y;
-			x = LOWORD(lParam);
-			y = HIWORD(lParam);
-
-
-			//MessageBox(hWnd, TEXT(""), TEXT(""), MB_OK);
-			g_rectRight = x;
-			g_rectBottom = y;
-
-
-			LPTSTR lptext = TEXT("Тестовое сообщение");
-
-			tRect = Rect;
-			tRect.left = x;
-			tRect.top = y;
-			tRect.right = tRect.left+150;
-			tRect.bottom= tRect.top+20;
-
-			InvalidateRect(hWnd, &Rect, TRUE);//и тут
-
-			//Rectangle(hDc, g_rectLeft, g_rectTop, g_rectRight, g_rectBottom);		
-			Ellipse(hDc, g_rectLeft, g_rectTop, g_rectRight, g_rectBottom);
-			//DrawText(hDc, lptext, lstrlen(lptext), &tRect, DT_CENTER);
-		}
-	}
-	return 0;
 
 	case WM_PAINT:    // Вывод при обновлении окна
 	{
 		PAINTSTRUCT ps;
-		hDC = BeginPaint(hWnd, &ps); // Получение контекста для
-									 // обновления окна
-		GetClientRect(hWnd, &Rect);//передаем указатель на нее
-
-
-		LPTSTR lptext = TEXT("Прямоугольная область текста");
-
-		DrawText(hDC, lptext,lstrlen(lptext),&Rect, DT_CENTER);
-
-		//Rectangle(hDC, g_rectLeft, g_rectTop, g_rectRight, g_rectBottom);
-		Ellipse(hDC, g_rectLeft, g_rectTop, g_rectRight, g_rectBottom);
+		hDC = BeginPaint(hWnd, &ps); // Получение контекста для обновления окна
 		EndPaint(hWnd, &ps); // Завершение обновления окна
 	}
 	return 0;
@@ -198,11 +94,13 @@ LRESULT CALLBACK SimWndProc(HWND hWnd, UINT msg,
 	{
 
 		int iRetValue1, iRetValue2;
-		LPCTSTR lpszMesBoxTitle = TEXT("Изучаем MessageBox");
+		LPCTSTR lpszMesBoxTitle = TEXT("Вариант 1");
+		LPCTSTR lpszMesBoxText = TEXT("The message box contains three push buttons: Abort, Retry, and Ignore.");
 		LPTSTR lpszResponce;
+
 		do {
-			iRetValue1 = MessageBox(hWnd, TEXT("The message box contains three push buttons: Abort, Retry, and Ignore."), lpszMesBoxTitle,
-				MB_ABORTRETRYIGNORE | MB_ICONEXCLAMATION);
+			iRetValue1 = MessageBox(hWnd, lpszMesBoxText, lpszMesBoxTitle, MB_ABORTRETRYIGNORE | MB_ICONEXCLAMATION);
+			
 			switch (iRetValue1)
 			{
 			case IDABORT:
---
+++ Lab_01/sp_pr1.h.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define MESSAGE_TEXT  TEXT("Hello World Win32 GUI Application")
+#define MESSAGE_TITLE TEXT("Окно сообщения") 
