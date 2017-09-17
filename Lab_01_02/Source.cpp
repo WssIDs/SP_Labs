0ff72f4afbc48fcb0cc4573d9c59b34d0c2089b6
WssIDs <wssids49@gmail.com>
2017-09-17 18:13:36 +0300

    Индивидуальное задание №2

--- Lab_01_02/Source.cpp
+++ Lab_01_02/Source.cpp
@@ -2,13 +2,143 @@
 #include <tchar.h>
 
 
+#include <Windows.h>
+#include <tchar.h>
+
+//-- Prototypes -------------------
+LRESULT CALLBACK Pr2_WndProc(HWND, UINT, WPARAM, LPARAM);
 
 
+//-- Global Variables ------------ 
+LPTSTR g_lpszClassName = TEXT("sp_pr2_class");
+LPTSTR g_lpszAplicationTitle = TEXT("Разработчик: Володько Виталий, 60331-1");
+LPTSTR g_lpszDestroyTitle = TEXT("Вариант 1");
+LPTSTR  g_lpszDestroyMessage = TEXT("Данный вывод выполнен в обработчике сообщения WM_DESTROY. Сообщение поступило от Windows в связи с разрушением окна.");
 
-
-//  Функция входа в программу 
-int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpszCmdLine, int nCmdShow)
+//  Стартовая функция 
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPTSTR lpszCmdLine, int nCmdShow)
 {
+	WNDCLASSEX wc;
+	MSG msg;
+	HWND hWnd;
 
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpszClassName = g_lpszClassName;
+	wc.lpfnWndProc = Pr2_WndProc;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, MAKEINTRESOURCE(IDI_APPLICATION));
+	wc.hCursor = LoadCursor(NULL, MAKEINTRESOURCE(IDC_ARROW));
+	HBRUSH hbr;
+	hbr = CreateSolidBrush(RGB(255, 0, 0));
+	wc.hbrBackground = hbr;
+
+	wc.lpszMenuName = NULL;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, TEXT("Ошибка регистрации класса окна!"),
+			TEXT("Ошибка"), MB_OK | MB_ICONERROR);
+		return FALSE;
+	}
+
+	DWORD Style;
+	Style = WS_OVERLAPPEDWINDOW^WS_MINIMIZEBOX;
+
+	hWnd = CreateWindowEx(NULL, wc.lpszClassName,
+		g_lpszAplicationTitle,
+		Style,
+		200,
+		200,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL, TEXT("Окно не создано!"),
+			TEXT("Ошибка"), MB_OK | MB_ICONERROR);
+		return FALSE;
+	}
+
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+// Оконная процедура
+LRESULT CALLBACK Pr2_WndProc(HWND hWnd, UINT msg,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hDC;
+
+	switch (msg)
+	{
+	case WM_CREATE:
+	{
+		MessageBox(hWnd, TEXT("Выполняется обработка WM_CREATE"), g_lpszDestroyTitle, MB_OK | MB_ICONEXCLAMATION);
+	}
 	return 0;
+	
+	case WM_LBUTTONDOWN:
+	{
+		HDC hdc = GetDC(hWnd);//получить контекст
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		rect.left = 50; 
+		rect.top = 50;
+		rect.bottom = rect.top + 300;
+
+		TCHAR lpszBuff[200];
+		int x = LOWORD(lParam);
+		int y = HIWORD(lParam);
+
+		wsprintf(lpszBuff, TEXT("Обработка сообщения WM_LBUTTONDOWN, которое посылается в окно при щелчке левой кнопки мыши."), TEXT("Hello, O World!"));
+		//TextOut(hdc, x, y, lpszBuff, lstrlen(lpszBuff));
+
+		DrawText(hdc, lpszBuff, -1, &rect, DT_CENTER);
+
+		ReleaseDC(hWnd, hdc);//Освободить контекст
+	}
+	return 0;
+
+	case WM_PAINT:    // Вывод при обновлении окна
+	{
+		PAINTSTRUCT ps;
+		hDC = BeginPaint(hWnd, &ps); // Получение контекста для обновления окна
+
+		TCHAR lpszBuff[200];
+		wsprintf(lpszBuff, TEXT("Вывод текста при обработке сообщения WM_PAINT. Это соообщение окно получает после того, как оно было закрыто другим окном и затем открыто."), TEXT("Hello, O World!"));
+		TextOut(hDC, 20, 100, lpszBuff, lstrlen(lpszBuff));
+
+		EndPaint(hWnd, &ps); // Завершение обновления окна
+	}
+	return 0;
+
+	case WM_DESTROY:  // Завершение работы приложения
+	{
+		MessageBox(hWnd, g_lpszDestroyMessage, g_lpszDestroyTitle, MB_OK | MB_ICONEXCLAMATION);
+
+		PostQuitMessage(0); // Посылка WM_QUIT приложению
+	}
+	return 0;
+
+	default: // Вызов "Обработчика по умолчанию"
+		return(DefWindowProc(hWnd, msg, wParam, lParam));
+	}
+	return FALSE;// Для ветвей с "break"
 }
